eb6f8af71607c47945c1ddb21fd8137b94a77b96
Efrain <carlosefrainchallo1@gmail.com>
2026-05-08 18:57:26 -0500

    18:56 - 08/05/2026 - Se agrega la siguiente clase de CISCO 2 que es PIP

---
+++ 13. PIP/ej1.py
@@ -0,0 +1,8 @@
+""" En este caso utilizamos (Instalada en la cnsola Git) el numpy """
+import numpy as np
+
+numeros = [1, 2, 3, 4, 5]
+print(np.mean(numeros))    # → 3.0  (promedio)
+print(np.sum(numeros))     # → 15   (suma)
+print(np.max(numeros))     # → 5    (máximo)
+print(np.min(numeros))     # → 1    (mínimo)
